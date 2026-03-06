e7f7d1ab17f538c5dc63784f6279b018620496dd
jyyyyy4 <annajy1425@gmail.com>
2026-03-06 12:20:04 +0900

    03.06

--- 08_exception/main.py
+++ 08_exception/main.py
@@ -1,5 +1,8 @@
 from exception_basic import demo_runtime_exception, demo_try_except_basics
+from exception_flow import demo_else_finally_file, demo_raise_custom_exception
 
 if __name__ == "__main__":
     # demo_runtime_exception()
-    demo_try_except_basics()
+    # demo_try_except_basics()
+    # demo_else_finally_file()
+    demo_raise_custom_exception()
